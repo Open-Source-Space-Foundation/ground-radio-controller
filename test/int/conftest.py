5e91693fe6c5597b2348df2496237847bd86effa
Jack Pearson <jack@pearson.onl>
2026-02-20 13:01:46 -0800

    add two-board data port test + bft

--- test/int/conftest.py
+++ test/int/conftest.py
@@ -5,10 +5,20 @@ def pytest_addoption(parser):
         default=[],
         help="list of data_port_ones to pass to test functions",
     )
+    parser.addoption(
+        "--data-port-two",
+        action="append",
+        default=[],
+        help="list of data_port_twos to pass to test functions",
+    )
 
 
 def pytest_generate_tests(metafunc):
     if "data_port_one" in metafunc.fixturenames:
         metafunc.parametrize(
             "data_port_one", metafunc.config.getoption("--data-port-one")
+        )
+    if "data_port_two" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "data_port_two", metafunc.config.getoption("--data-port-two")
         )
--- test/int/two_board_test.py
+++ test/int/two_board_test.py
@@ -3,12 +3,22 @@ These tests require another board be flashed with radio controller software and
 the connected PC is looping back data `cat /dev/ttyX >/dev/ttyX`
 """
 
+
+def test_open_data_ports(data_port_one, data_port_two):
+    tty1 = open(data_port_one)
+    tty1.close()
+    tty2 = open(data_port_one)
+    tty2.close()
+
+
 def test_write_increments_packets_received():
     # validate that 1 tx causes another rx on 1 board
     pass
 
+
 def test_full_loopback():
     # validate that data out = data in
     pass
+
 
 # Then test with changing doppler params
