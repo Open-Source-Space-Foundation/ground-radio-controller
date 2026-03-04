21f3f51bc77a83870690ab254f1384bedf9dfe69
Jack Pearson <jack@pearson.onl>
2026-03-04 13:46:15 -0800

    Revert "remove unneccessary sleep due to manual USB init"
    This reverts commit 27e7a37b148200c92ef1ee667058d248b9d28085.

--- FprimeZephyrReference/ReferenceDeployment/Main.cpp
+++ FprimeZephyrReference/ReferenceDeployment/Main.cpp
@@ -92,6 +92,12 @@ static int cdc_acm_serial_init_device(void) {
 int main(int argc, char* argv[]) {
     cdc_acm_serial_init_device();
 
+    // ** DO NOT REMOVE **//
+    //
+    // This sleep is necessary to allow the USB CDC ACM interface to initialize before
+    // the application starts writing to it.
+    k_sleep(K_MSEC(3000));
+
     Os::init();
     // Object for communicating state to the topology
     ReferenceDeployment::TopologyState inputs;
