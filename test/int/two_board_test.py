11fa42c7cd723a3877a8d5d6d6315d681812cf1c
Jack Pearson <jack@pearson.onl>
2026-04-07 11:06:55 -0700

    fix test_open_data_ports

--- test/int/two_board_test.py
+++ test/int/two_board_test.py
@@ -7,5 +7,5 @@ also connected to the PC. Should be run via `bft.sh`.
 def test_open_data_ports(data_port_one, data_port_two):
     tty1 = open(data_port_one)
     tty1.close()
-    tty2 = open(data_port_one)
+    tty2 = open(data_port_two)
     tty2.close()
