4594e8a93c95a1db91eac6191975a5a7be97b984
Jack Pearson <jack@pearson.onl>
2026-04-09 11:01:21 -0700

    add `test_create_and_remove_directory`

--- test/int/one_board_test.py
+++ test/int/one_board_test.py
@@ -18,14 +18,25 @@ def test_write_data_port(data_port_one):
         tty.write("\0")
 
 
-def test_create_directory(fprime_test_api):
+def test_create_and_remove_directory(fprime_test_api):
     dir_name = "/test_dir"
 
-    fprime_test_api.send_command(
+    fprime_test_api.send_and_assert_command(
         "ReferenceDeployment.fileManager.CreateDirectory",
         [dir_name],
+        timeout=2,
     )
     fprime_test_api.assert_event(
         "ReferenceDeployment.fileManager.CreateDirectorySucceeded",
+        timeout=2,
+    )
+
+    fprime_test_api.send_and_assert_command(
+        "ReferenceDeployment.fileManager.RemoveDirectory",
+        [dir_name],
+        timeout=2,
+    )
+    fprime_test_api.assert_event(
+        "ReferenceDeployment.fileManager.RemoveDirectorySucceeded",
         timeout=2,
     )
