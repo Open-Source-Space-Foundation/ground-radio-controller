5bbf5e827fcee60ca7d368a8d280b8b4826dca73
Jack Pearson <jack@pearson.onl>
2026-06-03 21:52:18 -0400

    add test_link_two_to_one_4096_bytes_one_byte_writes

--- test/two-board/two_board_test.py
+++ test/two-board/two_board_test.py
@@ -76,6 +76,28 @@ def test_link_one_to_two_4096_bytes_one_byte_writes(
         _assert_no_warnings(fprime_test_api)
 
 
+def test_link_two_to_one_4096_bytes_one_byte_writes(
+    fprime_test_api, data_port_one, data_port_two
+):
+    try:
+        # See previous test for explanation of magic numbers
+        sent = bytes(range(256)) * 16
+        bitrate = 2000
+        byte_period_seconds = 8 / bitrate
+
+        for byte in sent:
+            assert data_port_two.write(bytes([byte])) == 1
+            data_port_two.flush()
+            time.sleep(byte_period_seconds)
+
+        received = data_port_one.read(len(sent))
+        assert received == sent, (
+            f"Expected {len(sent)} bytes from board two, received {len(received)}"
+        )
+    finally:
+        _assert_no_warnings(fprime_test_api)
+
+
 def test_link_one_to_two_six_252_byte_packets(
     fprime_test_api, data_port_one, data_port_two
 ):
